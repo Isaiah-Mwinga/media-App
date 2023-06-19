31385a90f0ac16310299be2a30d9c54d1d49ee5d
Isaiah-Mwinga <motongori@marketforce360.com>
2023-06-19 21:04:09 +0300

    media app

--- media/src/main.jsx
+++ media/src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Login from './pages/login/Login.jsx'
+import Register from './pages/register/Register.jsx'
+import Profile from './pages/profile/Profile.jsx'
+import Home from './pages/home/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Home />
+    <Login />
+    <Register />
+    <Profile />
   </React.StrictMode>,
 )
