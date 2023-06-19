c3d2c19743fd35dc7a5602a412ebb890b8e96c02
Isaiah-Mwinga <motongori@marketforce360.com>
2023-06-19 21:11:31 +0300

    media app

--- media/src/pages/login/Login.jsx
+++ media/src/pages/login/Login.jsx
@@ -3,7 +3,25 @@ import "./Login.scss"
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>
+        <div className="card">
+            <div className="left">
+                <h1>Hello World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                <span>Don't have an account?</span>
+                <button>Sign Up</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Login</button>
+                    <span>Forgot Password?</span>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
