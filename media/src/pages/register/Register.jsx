c4e911c213347eb6e9e23e8da0949a73f1dd844d
Isaiah-Mwinga <motongori@marketforce360.com>
2023-06-23 17:01:24 +0300

    media app

--- media/src/pages/register/Register.jsx
+++ media/src/pages/register/Register.jsx
@@ -3,7 +3,24 @@ import "./Register.scss"
 
 const Register = () => {
   return (
-    <div className='register'>Register</div>
+    <div className='Register'>
+        <div className="card">
+            <div className="left">
+                <h1>Hello World</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                <span>have an account?</span>
+                <button>Sign Up</button>
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form>
+                    <input type="text" placeholder="Username" />
+                    <input type="password" placeholder="Password" />
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
