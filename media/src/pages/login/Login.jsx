df9313f0be7b68ef4eb290ab41cc72e67c336597
Isaiah-Mwinga <motongori@marketforce360.com>
2023-07-03 15:11:08 +0300

    media app

--- media/src/pages/login/Login.jsx
+++ media/src/pages/login/Login.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import "./Login.scss"
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/authContext";
+import "./login.scss";
 
 const Login = () => {
-  return (
-    <div className='login'>
-        <div className="card">
-            <div className="left">
-                <h1>Hello World</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                <span>Don't have an account?</span>
-                <button>Sign Up</button>
-            </div>
-            <div className="right">
-                <h1>Login</h1>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <button>Login</button>
-                    <span>Forgot Password?</span>
-                </form>
-            </div>
-        </div>
-    </div>
-  )
-}
+  const { login } = useContext(AuthContext);
 
-export default Login
+  const handleLogin = () => {
+    login();
+  };
+
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button onClick={handleLogin}>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
