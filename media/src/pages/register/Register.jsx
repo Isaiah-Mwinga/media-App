33aafb38c59c7c0b833e56abc787d24d0d2acde0
Isaiah-Mwinga <motongori@marketforce360.com>
2023-07-03 15:12:26 +0300

    media app

--- media/src/pages/register/Register.jsx
+++ media/src/pages/register/Register.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
-import "./Register.scss"
+import { Link } from "react-router-dom";
+import "./register.scss";
 
 const Register = () => {
   return (
-    <div className='Register'>
-        <div className="card">
-            <div className="left">
-                <h1>Hello World</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                <span>have an account?</span>
-                <button>Sign Up</button>
-            </div>
-            <div className="right">
-                <h1>Register</h1>
-                <form>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
-                    <input type="email" placeholder="email" />
-                    <input type="text" placeholder="Name" />          
-                    <button>Register</button>
-                </form>
-            </div>
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lama Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Do you have an account?</span>
+          <Link to="/login">
+          <button>Login</button>
+          </Link>
         </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
